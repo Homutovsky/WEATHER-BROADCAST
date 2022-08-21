d7b4ef16b02bef01f4f46d17cfdf6e1dddcea1b4
Вадим Хомутовский <vadimhomutovsky@gmail.com>
2022-08-21 22:49:19 +0300

    fix wrongs

--- appEventListener.js
+++ appEventListener.js
@@ -1,108 +1,105 @@
-import { headerBtn, footerInfo, footerImg, mainInput, mainBtn, weatherKey } from './nodes.js';
-import { request } from './index.js';
-import { images } from './getImages.js';
-import { getCityCordinates } from './getCityCordinates.js';
+import {
+	headerBtn,
+	footerInfo,
+	footerImg,
+	mainInput,
+	mainBtn,
+	weatherKey,
+} from "./nodes.js";
+import { request } from "./index.js";
+import { images } from "./getImages.js";
+import {
+	getCityCordinates,
+	cityLatitude,
+	cityLongitude,
+	localLatitude,
+	localLongitude,
+} from "./getCordinates.js";
 export let city;
 
-
 export const giveEventListener = function () {
-    request();
+	request();
 
-    let gitAvatar;
-    let gitName;
-    let gitBio;
-    let createDiv;
-    let i = 0;
-    let j = 0;
+	let gitAvatar;
+	let gitName;
+	let gitBio;
+	let createDiv;
+	let i = 0;
+	let j = 0;
 
-    request('https://api.github.com/users/homutovsky')
-        .then((data) => {
-            gitAvatar = data.avatar_url;
-            gitName = data.name;
-            gitBio = data.bio;
-            gitBio ? gitBio : gitBio = 'Front-end developer'
-        })
+	request("https://api.github.com/users/homutovsky").then((data) => {
+		gitAvatar = data.avatar_url;
+		gitName = data.name;
+		gitBio = data.bio;
+		gitBio ? gitBio : (gitBio = "Front-end developer");
+	});
 
-    footerInfo.addEventListener('mouseover', () => {
-        footerImg.src = gitAvatar;
-        const newElement = document.createElement('div');
-        footerInfo.after(newElement);
-        newElement.className = 'create-div';
-        createDiv = document.querySelector(".create-div");
-        createDiv.textContent = `${gitName} - ${gitBio}`;
-    })
+	footerInfo.addEventListener("mouseover", () => {
+		footerImg.src = gitAvatar;
+		const newElement = document.createElement("div");
+		footerInfo.after(newElement);
+		newElement.className = "create-div";
+		createDiv = document.querySelector(".create-div");
+		createDiv.textContent = `${gitName} - ${gitBio}`;
+	});
 
-    footerInfo.addEventListener('mouseout', () => {
-        footerImg.src = "img/footer-git_icon.svg";
-        createDiv.remove()
-    })
+	footerInfo.addEventListener("mouseout", () => {
+		footerImg.src = "img/footer-git_icon.svg";
+		createDiv.remove();
+	});
 
-    headerBtn.addEventListener('click', () => {
-        i++;
-        j++;
-        if (images[i]) {
-            const img = new Image();
-            img.src = images[i]
-            img.addEventListener('load', () => {
-                document.body.style.backgroundImage = `url('${images[i]}')`;
-            })
-        }
-    })
+	headerBtn.addEventListener("click", () => {
+		i++;
+		j++;
+		if (images[i]) {
+			const img = new Image();
+			img.src = images[i];
+			img.addEventListener("load", () => {
+				document.body.style.backgroundImage = `url('${images[i]}')`;
+			});
+		}
+	});
 
+	mainInput.addEventListener("input", (event) => {
+		if (mainInput.value) {
+			mainBtn.disabled = false;
+		} else {
+			mainBtn.disabled = true;
+		}
+		city = mainInput.value;
+	});
 
-    mainInput.addEventListener('input', (event) => {
-        if (mainInput.value) {
-            mainBtn.disabled = false;
-        } else {
-            mainBtn.disabled = true;
-        }
-        city = mainInput.value;
-    })
+	mainBtn.addEventListener("click", () => {
+		getCityCordinates();
+	});
 
-    mainBtn.addEventListener('click', () => {
-        getCityCordinates()
-    })
+	document.addEventListener("keydown", (event) => {
+		if (event.code === "Enter" && city !== "") {
+			getCityCordinates();
+		}
+	});
 
-    document.addEventListener('keydown', event => {
-        if (event.code === 'Enter' && city !== '') {
-            getCityCordinates()
-        }
-    })
+	const getWeatherForecast = function (Latitude, Longitude) {
+		Latitude = cityLatitude ? cityLatitude : localLatitude;
+		Longitude = cityLongitude ? cityLongitude : localLongitude;
+		console.log(
+			cityLatitude[0],
+			cityLongitude,
+			localLatitude,
+			localLongitude
+		);
 
-    let localLatitude;
-    let localLongitude;
-    navigator.geolocation.getCurrentPosition((succes) => {
-        if (succes) {
-            const { latitude: lat,
-                longitude: lng } = succes.coords;
-            localLatitude = lat;
-            localLongitude = lng;
-        } else {
-            localLatitude = '53.90336';
-            localLongitude = '27.5120128';
-        }
-    })
-
-    const getWeatherForecast = function () {
-        const weatherQuery = {
-            url: 'https://api.openweathermap.org/',
-            getRequestUrl() {
-                return (`${this.url}/data/3.0/onecall?lat=${localLatitude}&lon=${localLongitude}&exclude='minutely'&appid=${weatherKey}`);
-            }
-        }
-        request(weatherQuery.getRequestUrl())
-            .then(date => {
-                console.log(date, 1)
-            })
-
-    }
-    getWeatherForecast()
-
-
-
-
-}
-
-
-
-
+		const weatherQuery = {
+			url: "https://api.openweathermap.org/",
+			getRequestUrl() {
+				return `${this.url}/data/2.5/weather?lat=${Latitude}&lon=${Longitude}&appid=${weatherKey}`;
+			},
+		};
+		request(weatherQuery.getRequestUrl()).then((date) => {
+			console.log(date, 1);
+		});
+	};
+	setTimeout(() => {
+		getWeatherForecast();
+	}, 1000);
+};
--- index.js
+++ index.js
@@ -1,12 +1,22 @@
-import { giveEventListener } from './appEventListener.js';
-import { clockTimer } from './dateAndTime.js';
-import { timeToDay } from './dateAndTime.js';
-
-
+import { giveEventListener } from "./appEventListener.js";
+import { clockTimer } from "./dateAndTime.js";
+import { timeToDay } from "./dateAndTime.js";
+import {
+	cityLatitude,
+	cityLongitude,
+	localLatitude,
+	localLongitude,
+	getLocalPosition,
+} from "./getCordinates.js";
+getLocalPosition();
 giveEventListener();
 clockTimer();
-
 export function request(url) {
-    return fetch(url)
-        .then(response => response.json())
+	return fetch(url).then((response) => response.json());
 }
+
+cityLatitude.join("");
+cityLongitude.join("");
+localLatitude.join("");
+localLongitude.join("");
+console.log(cityLatitude, cityLongitude, localLatitude, localLongitude);
